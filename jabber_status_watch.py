415bf72056f73c35733e24498d1b8a10a2190ef6
Schnabel <703405268@ssxfsptritsa004.sanfordhealth.org>
2019-06-21 14:38:44 -0500

    Added code to start supporting command line options and pulled out printing output without the "-v" argument. Added error catching for the Type mis-match when the Jabber window is on a space that is not in the forefront.  Will continue to test to see if this fix is enough. Maybe will add a timer to switch to the window every few minutes and switch back?
    fixes #2

--- jabber_status_watch.py
+++ jabber_status_watch.py
@@ -1,6 +1,6 @@
 from subprocess import Popen, PIPE
 from blynclight import BlyncLight, BlyncLightNotFound
-import sys, signal
+import sys, signal, getopt
 import datetime
 
 def signal_handler(signal, frame):
@@ -8,6 +8,27 @@ def signal_handler(signal, frame):
     sys.exit(0)
 
 signal.signal(signal.SIGINT, signal_handler)
+
+Full_Command_Line_Options = sys.argv
+Arg_List = Full_Command_Line_Options[1:]
+unixOptions = "ho:v"
+gnuOptions = ["help", "output=", "verbose"]
+verbosity = False
+
+try:
+    arguments, values = getopt.getopt(Arg_List, unixOptions, gnuOptions)
+except getopt.error as err:
+    # output error, and return with an error code
+    print (str(err))
+    sys.exit(2)
+
+for currentArgument, currentValue in arguments:
+    if currentArgument in ("-v", "--verbose"):
+        print ("Verbose Output enabled...")
+        verbosity = True
+    elif currentArgument in ("-h", "--help"):
+        print ("-v = Verbose output")
+        sys.exit(0)
 
 red, blue, green, yellow, purple = (255, 0, 0), (0, 255, 0), (0, 0, 255), (254, 220, 86), (153,0,255)
 Status_Color = {"Available": green, "Away": yellow,"Busy": red,"On a Call": red, "Presenting": red, "In a meeting": red, "Do Not Disturb": purple}
@@ -34,24 +55,29 @@ while (True):
     Current_Jabber_Status = str(stdout).strip()
 
     if Previous_Jabber_Status != Current_Jabber_Status :
-        print ("----------------------------------")
-        print ("Jabber status changed from:",str(Previous_Jabber_Status)," to:",str(Current_Jabber_Status))
-        print ("----------------------------------")
+        if verbosity == True:
+            print ("----------------------------------")
+            print ("Jabber status changed from:",str(Previous_Jabber_Status)," to:",str(Current_Jabber_Status))
+            print ("----------------------------------")
+            now = datetime.datetime.now()
+            print ("Time:",now.strftime("%Y-%m-%d %H:%M:%S"))
         Previous_Jabber_Status = Current_Jabber_Status
-        now = datetime.datetime.now()
-        print ("Time:",now.strftime("%Y-%m-%d %H:%M:%S"))
         # Need to catch if status is blank "" and default to a color.
         Light_Color = Status_Color.get(str(Current_Jabber_Status))
         Light_Flash = Status_Flash.get(str(Current_Jabber_Status))
         Light_Flash_Speed = Status_Flash_Speed.get(str(Current_Jabber_Status))
-
-        print ("Light Color:",str(Light_Color))
-        print ("Light Flash:",str(Light_Flash))
-        print ("Light Flash Speed:",str(Light_Flash_Speed))
-
-        light.on = True
-        light.bright = True
-        light.color= Light_Color
-        light.flash = Light_Flash
-
-        print ("Light Status:\n",str(light))
+        #fix for issue 2, Type Error thrown when Jabber window is in the non-active "Space"
+        try:
+            light.on = True
+            light.bright = True
+            light.color= Light_Color
+            light.flash = Light_Flash
+            if verbosity == True:
+                print ("Light Color:",str(Light_Color))
+                print ("Light Flash:",str(Light_Flash))
+                print ("Light Flash Speed:",str(Light_Flash_Speed))
+                print ("Light Status:\n",str(light))
+        except TypeError:
+            if verbosity == True:
+                print ("Jabber Window in the background space, so we can't pull the status. Continuing.")
+            pass
